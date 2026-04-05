564f83956bae8d91d31fa33ccafb3f821611eea3
Ahmed Hany <ahany9710@gmail.com>
2026-04-05 20:54:12 +0200

    dafadfda

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { lazy, Suspense } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { lazy, Suspense, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import FloatingActionBar from './components/FloatingActionBar';
 import Landing from './pages/Landing';
+import { trackMetaPageView } from './utils/metaPixel';
 
 const ThankYou = lazy(() => import('./pages/ThankYou'));
+
+function MetaPixelPageViews() {
+  const location = useLocation();
+  useEffect(() => {
+    trackMetaPageView();
+  }, [location.pathname]);
+  return null;
+}
 
 function App() {
   return (
     <BrowserRouter>
+      <MetaPixelPageViews />
       <Routes>
         <Route
           path="/"
